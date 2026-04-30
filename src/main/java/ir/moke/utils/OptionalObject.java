cd1eed95a9ec98e8175e399b320f9723af426ca0
mah454 <mah454@gmail.com>
2026-04-30 21:11:48 +0330

    Change Class

--- src/main/java/ir/moke/utils/OptionalObject.java
+++ src/main/java/ir/moke/utils/OptionalObject.java
@@ -7,23 +7,23 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-public class OptionalString {
+public class OptionalObject<T> {
 
-    private final String value;
+    private final T value;
 
-    private OptionalString(String value) {
+    private OptionalObject(T value) {
         this.value = value;
     }
 
-    public static OptionalString of(String value) {
-        return new OptionalString(value);
+    public static <T> OptionalObject<T> of(T value) {
+        return new OptionalObject<T>(value);
     }
 
-    public boolean check(Predicate<String> predicate) {
+    public boolean check(Predicate<? super T> predicate) {
         return predicate.test(this.value);
     }
 
-    public <U> U parse(Predicate<String> predicate, Function<String, ? extends U> parser) {
+    public <U> U parse(Predicate<T> predicate, Function<? super T, ? extends U> parser) {
         if (check(predicate)) {
             return Objects.requireNonNull(parser).apply(this.value);
         } else {
@@ -31,15 +31,15 @@ public class OptionalString {
         }
     }
 
-    public <U> U parseOrGet(Predicate<String> predicate, Function<String, ? extends U> parser, Supplier<? extends U> supplier) {
+    public <U> U parseOrGet(Predicate<T> predicate, Function<? super T, ? extends U> parser, Supplier<? extends U> supplier) {
         if (check(predicate)) {
             return Objects.requireNonNull(parser).apply(this.value);
         } else {
-            return Objects.requireNonNull(supplier).get();
+            return supplier == null ? null : supplier.get();
         }
     }
 
-    public <U> U parseOrGet(Predicate<String> predicate, Function<String, ? extends U> parser, U u) {
+    public <U> U parseOrGet(Predicate<? super T> predicate, Function<? super T, ? extends U> parser, U u) {
         if (check(predicate)) {
             return Objects.requireNonNull(parser).apply(this.value);
         } else {
